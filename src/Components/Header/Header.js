e81cfd6e0baf9e74ea3815af2adc3ce80d222165
mahmudul1995 <mahmuduls451@gmail.com>
2022-04-09 15:38:00 +0600

    added firebase auth state on login form

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,16 +3,17 @@ import { Link } from 'react-router-dom';
 import useFirebase from '../../hooks/useFirebase';
 
 const Header = () => {
-    const {user} =useFirebase();
+    const {user,handleSignOut} =useFirebase();
     return (
         <div className="m-7 shadow-lg m-auto p-5 ">
             <Link className="m-3 p-4 text-violet-800 font-bold hover:text-lime-400" to='/home'>Home</Link>
             <Link className="m-3 p-4 text-violet-800 font-bold hover:text-lime-400" to='/products'>Products</Link>
             <Link className="m-3 p-4 text-violet-800 font-bold hover:text-lime-400" to='/orders'>Orders</Link>
             <Link className="m-3 p-4 text-violet-800 font-bold hover:text-lime-400" to='/register'>Register</Link>
+            <span>{user ?.displayName && user.displayName}</span>
 
             {
-                user.uid? <button>Sign Out</button> :
+                user?.uid? <button className="font-semibold bg-yellow-700 text-white border-2 p-1 m-2 rounded-md" onClick={handleSignOut}>Sign Out</button> :
             <Link className="m-3 p-4 text-violet-800 font-bold hover:text-lime-400" to='/login'>Login</Link>
             }
 
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,5 +1,5 @@
-import  { useState } from 'react';
-import { getAuth, signInWithPopup } from "firebase/auth";
+import  { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
 import app from '../firebase.init'
 const auth= getAuth(app);
@@ -23,8 +23,21 @@ const useFirebase = () => {
            
           });
     }
+
+    const handleSignOut = (user) => {
+        signOut(auth)
+        .then(() => {
+
+        })
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth,user=>{
+            setUser(user)
+        })
+    },[])
     // return [user,setUser];
-    return {user,signInWithGoogle};
+    return {user,signInWithGoogle,handleSignOut};
 };
 
 export default useFirebase;
